dc575f0fdf4bf1934194eded693bcd340f7fa5b2
Ethan Todd <2464490T@student.gla.ac.uk>
2021-02-09 12:26:09 +0000

    Finished chapter 9, restricted add page/cat

--- rango/views.py
+++ rango/views.py
@@ -39,6 +39,7 @@ def show_category(request, category_name_slug):
     
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -51,6 +52,7 @@ def add_category(request):
             print(form.errors)
     return render(request,'rango/add_category.html',{'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -139,4 +141,4 @@ def user_logout(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
